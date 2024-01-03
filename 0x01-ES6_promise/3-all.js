5bff647604837eb33ead0f4a391a23d9d12a22fc
Eric Ubaga <ubaga91@gmail.com>
2024-01-04 00:38:04 +0300

    Added advanced task file

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,8 +4,8 @@ export default async function handleProfileSignup() {
   // Use Promise.all to resolve all promises
   return Promise.all([uploadPhoto(), createUser()])
     // Log body firstName lastName to the console
-    .then(([photo, user]) => {
-      console.log(photo.body, user.firstName, user.lastName);
+    .then((val) => {
+      console.log(val[0].body, val[1].firstName, val[1].lastName);
     })
     // In the event of an error, log Signup system offline to the console
     .catch(() => console.log('Signup system offline'));
