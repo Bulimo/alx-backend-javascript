6b99d837fab41ea79a3460c6884ad170924e834c
Eric Ubaga <ubaga91@gmail.com>
2024-01-04 01:16:23 +0300

    updtaed task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,18 +2,17 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
   // Use Promise.all to resolve all promises
-  return Promise.all([uploadPhoto(), createUser()])
-    // Log body firstName lastName to the console
-    .then((val) => {
-      console.log(val[0].body, val[1].firstName, val[1].lastName);
-    })
-    // In the event of an error, log Signup system offline to the console
-    .catch(() => console.log('Signup system offline'));
-  // try {
-  //   const photo = await uploadPhoto();
-  //   const user = await createUser();
-  //   console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  // } catch (e) {
-  //   console.error('Signup system offline');
-  // }
+  // return Promise.all([uploadPhoto(), createUser()])
+  //   // Log body firstName lastName to the console
+  //   .then((val) => {
+  //     console.log(val[0].body, val[1].firstName, val[1].lastName);
+  //   })
+  //   // In the event of an error, log Signup system offline to the console
+  //   .catch(() => console.log('Signup system offline'));
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (e) {
+    console.log('Signup system offline');
+  }
 }
