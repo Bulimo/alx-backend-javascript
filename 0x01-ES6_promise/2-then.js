c31ed70f183e825cdcf14f6aed0c33e902df13a4
Eric Ubaga <ubaga91@gmail.com>
2024-01-03 16:37:00 +0300

    Changed the implementation of task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,19 @@
 export default async function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({ status: 200, body: 'Success' }))
+  // return promise
+  //   .then(() => ({ status: 200, body: 'success' }))
+  //   .catch(() => (new Error()))
+  //   .then(() => console.log('Got a response from the API'));
+  promise.then(() => {
+    const obj = {
+      status: 200,
+      body: 'success',
+    };
+    return obj;
+  })
+    // When the Promise rejects, return an empty Error object
     .catch(() => (new Error()))
-    .then(() => console.log('Got a response from the API'));
+    // For every resolution, log Got a response from the API to the console
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
