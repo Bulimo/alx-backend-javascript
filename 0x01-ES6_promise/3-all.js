6fc18cc53c08af172c650caec31d8f299ac60a22
Eric Ubaga <ubaga91@gmail.com>
2024-01-04 01:22:37 +0300

    cleaned up task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  // Use Promise.all to resolve all promises
-  // return Promise.all([uploadPhoto(), createUser()])
-  //   // Log body firstName lastName to the console
-  //   .then((val) => {
-  //     console.log(val[0].body, val[1].firstName, val[1].lastName);
-  //   })
-  //   // In the event of an error, log Signup system offline to the console
-  //   .catch(() => console.log('Signup system offline'));
   try {
     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
