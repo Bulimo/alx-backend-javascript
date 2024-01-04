f3bc37a67656168b2011fa9ef29d71f718587281
Eric Ubaga <ubaga91@gmail.com>
2024-01-04 23:50:25 +0300

    Added call to method in the constructor

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -4,10 +4,11 @@ export default class Building {
       throw TypeError('sqft must must be a number');
     }
     this._sqft = sqft;
+    this.evacuationWarningMessage();
 
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-      throw new Error('Class extending Building must implement evacuationWarningMessage');
-    }
+    // if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+    //   throw new Error('Class extending Building must override evacuationWarningMessage');
+    // }
   }
 
   get sqft() {
@@ -16,6 +17,7 @@ export default class Building {
 
   // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
-    throw new Error('evacuationWarningMessage method must be implemented');
+    // Throw an error if the method is not overridden by a subclass
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
