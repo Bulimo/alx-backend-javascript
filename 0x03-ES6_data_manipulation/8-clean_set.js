fe97662247fe741a3f3fcf3ccd101b35f76c142a
Eric Ubaga <ubaga91@gmail.com>
2024-01-09 09:34:49 +0300

    Remove return empty string statement

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!(set instanceof Set) || typeof startString !== 'string') {
     throw new Error('Invalid set or startString');
   }
-  if (startString === '') return '';
+  // if (startString === '') return '';
   // const filteredValues = Array.from(set)
   //   .filter((value) => value.startsWith(startString))
   //   .map((value) => value.slice(startString.length))
