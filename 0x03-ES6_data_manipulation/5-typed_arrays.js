eb1aa6c836ccf594af6cd6a17483f33c5537e5e7
Eric Ubaga <ubaga91@gmail.com>
2024-01-09 09:02:54 +0300

    Returned dataview of the biffer in task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,5 +5,5 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8view = new Int8Array(buffer);
   int8view[position] = value;
-  return buffer;
+  return new DataView(buffer);
 }
