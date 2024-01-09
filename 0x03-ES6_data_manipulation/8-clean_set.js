5e090633c8a428b3c6a756f2cb436d06b9a3b9a3
Eric Ubaga <ubaga91@gmail.com>
2024-01-09 09:16:42 +0300

    Added error check for set input

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, startString) {
+  if (!(set instanceof Set)) throw new Error('Set must be of type Set');
   if (startString === '') return '';
   const filteredValues = Array.from(set)
     .filter((value) => value.startsWith(startString))
