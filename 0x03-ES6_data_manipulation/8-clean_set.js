3ac44fbefa594023e66c61d0ac974500a314dfcf
Eric Ubaga <ubaga91@gmail.com>
2024-01-09 09:30:44 +0300

    Added the checks for Set, startString and the values in the filter

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,14 @@
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set)) throw new Error('Set must be of type Set');
+  if (!(set instanceof Set) || typeof startString !== 'string') {
+    throw new Error('Invalid set or startString');
+  }
   if (startString === '') return '';
+  // const filteredValues = Array.from(set)
+  //   .filter((value) => value.startsWith(startString))
+  //   .map((value) => value.slice(startString.length))
+  //   .join('-');
   const filteredValues = Array.from(set)
-    .filter((value) => value.startsWith(startString))
+    .filter((value) => value && typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
 
