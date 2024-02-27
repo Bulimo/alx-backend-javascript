94e9cae8acfba162c8782d6a5c31e34d1088e0bb
Eric Ubaga <ubaga91@gmail.com>
2024-02-27 22:35:00 +0300

    Changed the logic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ displayMessage('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    displayMessage(`Your name is: ${chunk}`);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 }).on('end', () => {
   displayMessage('This important software is now closing');
