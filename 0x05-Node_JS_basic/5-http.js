06aca3b620c47eabb050b55767065b9de6296bb1
Eric Ubaga <ubaga91@gmail.com>
2024-02-27 22:55:07 +0300

    changed implementation

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,31 +1,31 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const hostname = 'localhost';
 const port = 1245;
 
-const file = process.argv[2];
-
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const students = await countStudents(file);
-      res.end(`${students.join('\n')}`);
-    } catch (e) {
-      res.end(e.message);
-    }
-    res.statusCode = 404;
-    res.end();
+const app = http.createServer((req, res) => {
+  switch (req.url) {
+    case '/':
+      res.writeHead(200);
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      res.writeHead(200);
+      res.write('This is the list of our students\n');
+      countStudents(process.argv[2])
+        .then((data) => {
+          res.end(data);
+        })
+        .catch((error) => {
+          res.end(error.message);
+        });
+      break;
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: 'Resource not found' }));
   }
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port);
 
 module.exports = app;
