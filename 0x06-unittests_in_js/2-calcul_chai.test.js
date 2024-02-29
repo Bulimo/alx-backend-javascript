d36cc8a58f5b2f65dabda28ed14771582317e0ea
Eric Ubaga <ubaga91@gmail.com>
2024-02-29 15:51:48 +0300

    Added tests

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -53,3 +53,30 @@ describe('calculateNumber', () => {
     });
   });
 });
+
+describe('calculateNumber', () => {
+  it('should round a and b and return by the type', () => {
+    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+  });
+  it('should round a and b and return by the type', () => {
+    expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+  });
+  it('should round a and b and return by the type', () => {
+    expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+  });
+  it('should round a and b and return by the type', () => {
+    expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
+  });
+  it('should round a and b and return by the type', () => {
+    expect(calculateNumber('SUM', 3.7, 1)).to.equal(5);
+  });
+  it('should round a and b and return by the type', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+  it('should round a and b and return by the type', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+  it('should round a and b and return by the type', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+});
